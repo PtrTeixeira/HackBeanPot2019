5e84b8df03110d0620efe6fd7b57e4051492a5fc
Peter Teixeira <PtrTeixeira@gmail.com>
2019-02-09 21:44:34 -0500

    Split out some of the frontend code; css

--- server.py
+++ server.py
@@ -3,16 +3,35 @@ import json
 
 app = Flask(__name__, static_url_path = "")
 
-@app.route("/load", methods=['POST'])
+@app.route("/api/load", methods=['POST'])
 def get_text():
+    # These get ignored for the moment :c
     usernames = request.get_json()['usernames']
     image_count = request.get_json()['imageCount']
 
-    print(usernames)
-    return jsonify({ "generated_caption": "this is two people sitting in a tree" })
+    return jsonify([
+        {
+            "caption": "this is the caption\nBonusCaption",
+            "image": "http://flask.pocoo.org/docs/1.0/_static/flask.png",
+            "video": False
+        },
+        {
+            "caption": "this is the caption\nBonusCaption",
+            "image": "http://flask.pocoo.org/docs/1.0/_static/flask.png",
+            "video": False
+        },
+        {
+            "caption": "this is the caption\nBonusCaption",
+            "image": "http://flask.pocoo.org/docs/1.0/_static/flask.png",
+            "video": False
+        }
+    ])
 
 @app.route("/")
-def serve_static():
+def server_index():
     return send_from_directory('www', 'index.html')
 
-
+@app.route("/static/<path:filename>")
+def serve_static(filename):
+    print(filename)
+    return send_from_directory('www', filename)
